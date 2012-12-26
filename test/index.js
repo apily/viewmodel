8d3d1fbac09c225d9882d9a0845cafb3b73f180d
Enrico Marino <enrico.marino@email.com>
2012-12-26 13:42:47 +0100

    added test

---
+++ test/index.js
@@ -0,0 +1,12 @@
+var delegates = require('viewmodel');
+var Emitter = require('model');
+
+(function(){
+  var el = document.getElementById('list-one');
+  var model = new Model();
+  var view = ViewModel(el, model);
+  
+  // view.messages.bind('change:size', 'onchange');
+  // view.events.bind('click li a', 'onclick');
+
+}());
